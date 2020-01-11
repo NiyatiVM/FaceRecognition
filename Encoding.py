9fb4bdaef979c7efe759cf9f3cfde2580c127799
NiyatiVM <45822929+NiyatiVM@users.noreply.github.com>
2020-01-11 14:46:33 +0530

    Updated to encode separately
    Encodings created for all people in train_dir

--- Encoding.py
+++ Encoding.py
@@ -3,12 +3,12 @@ import pickle
 import face_recognition
 from sklearn import svm
 import os,sys
+from sklearn.model_selection import train_test_split
 
 #Defining paths
 mainpath =r'C:\Users\Niyati\Desktop\FaceRecog\Phase-01'#Initialize the path 
 training_dir = os.listdir(os.path.join(mainpath,'train_dir'))
 train_dir = os.path.join(mainpath,'train_dir')
-
 #encodings and names
 encodings = []
 names = []
@@ -31,16 +31,18 @@ for person in training_dir:
 			print(person + "/" + person_img + " was skipped and can't be used for training because number of faces found is "+str(len(face_bounding_boxes)))
 	print("[INFO] serializing encodings...")
 	data={"encodings":encodings,"names":names}
+	#print(data)
 	encoding_dir=os.path.join(mainpath,"encodings")
 	if(os.path.exists(encoding_dir)):
 		pass
 	else:
 		os.mkdir(encoding_dir)
-	f=open(os.path.join(encoding_dir,"e"+str(person)+".pickle"),"wb")
-	pickle.dump(data,f)
-	f.close
-clf = svm.SVC(gamma='scale')
-clf.fit(encodings,names)
+	#f=open(os.path.join(encoding_dir,"e"+str(person)+".pickle"),"wb")
+f=open(os.path.join(encoding_dir,"encodings.pickle"),"wb")
+pickle.dump(data,f)
+f.close
+
+"""
 test_image = face_recognition.load_image_file('test_image.jpg')
 face_locations = face_recognition.face_locations(test_image)
 no = len(face_locations)
@@ -50,3 +52,4 @@ for i in range(no):
     test_image_enc = face_recognition.face_encodings(test_image)[i]
     name = clf.predict([test_image_enc])
     print(*name)
+"""
