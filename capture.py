06c8f6875e0aa741e99fab93861b18d76facd9ba
NiyatiVM <45822929+NiyatiVM@users.noreply.github.com>
2020-01-11 14:23:36 +0530

    Updated
    Shows boundary boxes and stores images after applying CLAHE

--- capture.py
+++ capture.py
@@ -56,24 +56,29 @@ while True:
         print("{} written!".format(img_name))
         img_counter += 1
 """
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 while True:
-       
-       if img_counter<5:
-              
-              k= cv2.waitKey(1)
-              ret,frame = cam.read()
-              cv2.imshow("Capture",frame)
-              if k%256 == 32:
-                     
-                     fileName = os.path.join(person_dir,"IMAGE "+str(img_counter)+".png")
-                     cv2.imwrite(fileName,frame)
-                     img_counter+=1
-                     print("Image captured")
-       else:
-
-              print("5 images Captured")
-              break
-			  
+	if img_counter<5:
+		k= cv2.waitKey(1)
+		ret,frame = cam.read()
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+		faces = faceCascade.detectMultiScale(gray,scaleFactor=1.3,minNeighbors=3,minSize=(30, 30))
+		#print("Found {0} Faces!".format(len(faces)))
+		for (x, y, w, h) in faces:
+			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+			cv2.imshow("Capture",frame)
+			if k%256 == 32:
+				roi_color = frame[y:y + h, x:x + w]
+				fileName = os.path.join(person_dir,"IMAGE "+str(img_counter)+".png")
+				new_img=cv2.cvtColor(roi_color, cv2.COLOR_BGR2GRAY)
+				cll = clahe.apply(new_img)
+				cv2.imwrite(fileName,cll)
+				img_counter+=1
+				print("Image captured")
+	else:
+		print("5 images Captured")
+		break
 cam.release()
 
 cv2.destroyAllWindows()
